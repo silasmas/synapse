70a9bb0abf102b2650c6cab99251839e29c0e7e7
silasmas <silasjmas@gmail.com>
2022-10-20 23:03:32 +0100

    kfjfj

--- resources/views/site/parties/templatedetail.blade.php
+++ resources/views/site/parties/templatedetail.blade.php
@@ -137,11 +137,20 @@
                     </a>
                 </div>
                 <div class="offcanvas-text">
-                    <p>We denounce with righteous indig nation in and dislike men who are so beguiled and to demo
-                        realized, that they data forest see best business consulting wordpress theme 2021.</p>
+                    <p>
+                        Nous visons à transformer les potentiels de la RDC et de l'Afrique en
+                        richesse stable et durable au travers nos différents secteurs
+                        d’activités.
+                        Nous sommes appelés à autonomiser et de soutenir les petits et
+                        moyens producteurs alimentaires africains engagés dans la lutte
+                        contre la pauvreté, le chômage et l'insécurité alimentaire à travers
+                        notre modèle commercial principal par abonnement, une solution
+                        basée sur une configuration sur base d’une répartition des dividendes
+                        de manière équitable.
+                    </p>
                 </div>
                 <div class="media-img">
-                    <img src="assets/images/off2.jpg" alt="Images">
+                    <img src="{{ asset('assets/images/off2.jpg') }}" alt="Images">
                 </div>
                 <div class="canvas-contact">
                     <div class="address-area">
@@ -150,8 +159,8 @@
                                 <i class="flaticon-location"></i>
                             </div>
                             <div class="info-content">
-                                <h4 class="title">Address</h4>
-                                <em>05 kandi BR. New York</em>
+                                <h4 class="title">Addresse</h4>
+                                <em>Imm botour ogefrem-Local 14, Kin-Gombe</em>
                             </div>
                         </div>
                         <div class="address-list">
@@ -160,23 +169,29 @@
                             </div>
                             <div class="info-content">
                                 <h4 class="title">Email</h4>
-                                <em><a href="mailto:support@rstheme.com">support@rstheme.com</a></em>
+                                <em><a href="mailto:info@groupsynapse.org">Info@groupsynapse.org</a></em>
                             </div>
-                        </div>
+                         </div>
                         <div class="address-list">
                             <div class="info-icon">
-                                <i class="flaticon-call"></i>
+                              <i class="flaticon-call"></i>
                             </div>
                             <div class="info-content">
-                                <h4 class="title">Phone</h4>
-                                <em>+019988772</em>
+                              <h4 class="title">Phone</h4>
+                              <em> <a href="tel:(+243)999930158">(+243)999930158</a></em>
                             </div>
                         </div>
                     </div>
                     <ul class="social">
-                        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                        <li><a href="#"><i class="fa fa-pinterest-p"></i></a></li>
+                        <li>
+                            <a href="https://web.facebook.com/profile.php?id=100080432703036" target="_blank"><span><i class="fa fa-facebook"></i></span></a>
+                        </li>
+                        <li>
+                            <a href="https://www.youtube.com/channel/UCQV81mSQQcrwTe44r7BQGLA" target="_blank"><span><i class="fa fa-youtube"></i></span></a>
+                        </li>
+                        <li>
+                            <a href="https://www.instagram.com/synapsegroup4/" target="_blank"><span><i class="fa fa-instagram"></i></span></a>
+                        </li>
                     </ul>
                 </div>
             </nav>
