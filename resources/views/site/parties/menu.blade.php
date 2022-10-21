6cc1f91fa9a833b80b2ba5362a7ededadc3327aa
silasmas <silasjmas@gmail.com>
2022-10-21 16:41:08 +0100

    fff

--- resources/views/site/parties/menu.blade.php
+++ resources/views/site/parties/menu.blade.php
@@ -1,13 +1,13 @@
 <div class="offwrap"></div>
 
 <!--Preloader start here-->
-{{-- <div id="pre-load">
+<div id="pre-load">
    <div id="loader" class="loader">
        <div class="loader-container">
            <div class='loader-icon'><img src="{{ asset('assets/images/logo.png') }}" alt="Group synapse"></div>
        </div>
    </div>
-</div> --}}
+</div>
 <!--Preloader area end here-->
 
 <!-- Main content Start -->
